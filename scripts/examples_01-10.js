c38b8cbcb54e93d73f83e29820ceab27d0cc7fe8
jchance5 <78717751+jchance5@users.noreply.github.com>
2022-09-12 17:38:05 -0400

    Create examples_01-10.js

---
+++ scripts/examples_01-10.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var welcome = "Welcome to GiveAnotherChanceEst!";
+var welcome2 = "Where computer scientists are given another chance in this established IT company."
+
+alert(welcome);
+alert(welcome2);
+
+var sum = "25";
+var sum2 = "1000";
+var unknown = sum * sum2;
+
+var nonLetterorNumber = _$;
+
+var total = 6 * 10 + 4 / 10 % 10;
+
+var newNum = total--;
+
+var num = 15;
+var num2 = 35;
+var num3 = 9;
+var newTotal = (num + num2) / num3;
+
+var choice = prompt("Are you interested?");
+
+alert("For more updates and information, contact us through phone or email.");
+alert("Have a blessed day.");
