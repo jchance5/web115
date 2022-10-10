12543ca84e3c962e61c5f61810a377f8c1346fac
jchance5 <78717751+jchance5@users.noreply.github.com>
2022-10-10 18:27:12 -0400

    Update fizzbuzz1.js

--- scripts/fizzbuzz1.js
+++ scripts/fizzbuzz1.js
@@ -3,10 +3,10 @@
 window.onload = function() {
   
   document.forms[0].onsubmit = function() {
-    var first_name=document.getElementById("fName").value;
-    var middle_name=document.getElementById("mName").value;
-    var last_name=document.getElementById("lName").value;
-    var welcome=document.getElementById("greeting").textContent;
+    var first_name = document.getElementById("fName").value;
+    var middle_name = document.getElementById("mName").value;
+    var last_name = document.getElementById("lName").value;
+    var welcome = document.getElementById("greeting").textContent;
     document.getElementById("greeting").innerHTML = welcome +" "+ first_name +" "+ middle_name + ". " + last_name;   
     let result = '';		
     for (var i = 1; i <= 140; i++) {
@@ -24,5 +24,6 @@ window.onload = function() {
 	     }
 	     result += "<br>";
     }
-	  document.getElementById('output').innerHTML = result;
+	  
+    document.getElementById('output').innerHTML = result;
 }
