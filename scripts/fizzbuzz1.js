b7e1bf2226f2843aacc01476b22647dec5a6133b
jchance5 <78717751+jchance5@users.noreply.github.com>
2022-10-18 18:01:11 -0400

    Update fizzbuzz1.js

--- scripts/fizzbuzz1.js
+++ scripts/fizzbuzz1.js
@@ -1,25 +1,31 @@
 "use strict";
 
 window.onload = function() {
-	const first = document.getElementById('firstName').value;
-	const mid = document.getElementById('middleInitial').value;
-	const last = document.getElementById('lastName').value;
-	document.getElementById('greeting').innerHTML = newGreeting;
-	let outputText = "";
-	for (var i = 1; i <= 140; i++) {
-        if (i % 3 != 0 && i % 5 != 0) {
-            outputText += i + " One";
-        }
-        else {
-            outputText += i.toString();
-            if (i % 3 == 0) {
-                outputText += " More";
-            }
-            if (i % 5 == 0) {
-                outputText += " Try";
-            }
-            outputText += "<br>";
-        }
-    } 
-	document.getElementById('output').innerHTML = outputText;
-}
+  
+  document.forms[0].onsubmit = function() {
+    var first_name=document.getElementById("fName").value;
+    var middle_name=document.getElementById("mName").value;
+    var last_name=document.getElementById("lName").value;
+    var welcome=document.getElementById("greeting").textContent;
+    document.getElementById("greeting").innerHTML = welcome +" "+ first_name +" "+ middle_name + ". " + last_name;
+    var words = 140;
+    var expect = "";
+    for (var i = 1; i <= words; i++) {
+	    if (i % 3 === 0 && i % 5 != 0) {
+				expect += i +".) "+ "One<br>";
+		}
+			else if (i % 5 === 0 && i % 3 != 0) {
+				expect +=  i +".) "+ "More<br>";
+		}
+			else if (i % 5 === 0 && i % 3 === 0) {
+				expect +=  i +".) "+ "Try<br>";
+		}
+			else {
+				expect +=  i +".) "+ "One more<br>";
+      }
+    }
+    
+    document.getElementById("displayR").innerHTML = expect;     
+    return false;
+  };
+};
