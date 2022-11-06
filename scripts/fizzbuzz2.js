587d1dce67fc21dc0eb9450ab2df756775a0ab00
jchance5 <78717751+jchance5@users.noreply.github.com>
2022-11-06 09:03:18 -0500

    Update fizzbuzz2.js

--- scripts/fizzbuzz2.js
+++ scripts/fizzbuzz2.js
@@ -1,34 +1,33 @@
 
-window.onload = function() {
-  
-  document.forms[0].onsubmit = function() {
-    var first_name=document.getElementById("fName").value;
-    var middle_name=document.getElementById("mName").value;
-    var last_name=document.getElementById("lName").value;
+function greeting() {   
+    var fName = document.getElementById("fName").value;
+    var mName = document.getElementById("mName").value;
+    var lName = document.getElementById("lName").value;
     alert("Hello! " + fName + " " +  mName + ". " + lName + ".");
     var firstNum = parseInt(prompt("Please enter the first number? "));
     var secondNum = parseInt(prompt("Please enter the second number? "));
-    var words = 140;
-    var expect = "";
-    for (var i = 1; i <= words; i++) {
-	    if (displayWords(i,firstNum) && displayWords(i,secondNum)) {
-				values += i +". "+ "One<br>";
-		}
-			else if (displayWords(i,firstNum) && ! displayWords(i,secondNum)) {
-				values += i +". "+ "More<br>";
-		}
-			else if (! displayWords(i,firstNum) && displayWords(i,secondNum)) {
-				values += i +". "+ "Try<br>";
-		}
-			else {
-				values += i +". "+ "One more<br>";
-      }
+    var values = "";
+    var numLists = 140;
+
+ for (var i = 1; i <= numLists; i++) { 
+
+    if (displayWords(i,firstNum) && displayWords(i,secondNum)) {
+        values += i +". "+ "One<br>";
     }
-    
-    document.getElementById("displayR").innerHTML = expect;     
-    return false;
-  }
-  function displayWords (firstNum, secondNum) {
+    else if (displayWords(i,firstNum) && ! displayWords(i,secondNum)) {
+        values += i +". "+ "More<br>";
+    }
+    else if (! displayWords(i,firstNum) && displayWords(i,secondNum)) {
+        values += i +". "+ "Try<br>";
+    }
+    else {
+        values += i +". "+ "One more<br>";
+    }  
+ } 
+    document.getElementById("mycounter").innerHTML = values;     
+       return false;     
+}
+function displayWords (firstNum, secondNum) {
     if (firstNum % secondNum === 0) {
       return true;
       }
@@ -40,5 +39,5 @@ function changehead() {
     var fName = document.getElementById("fName").value;
     var mName = document.getElementById("mName").value;
     var lName = document.getElementById("lName").value;
-    document.getElementById("display").innerHTML = "Welcome to GiveAnotherChance Fizz Buzz," +fName+" "+mName+" "+lName;
+    document.getElementById("display").innerHTML = "Welcome to Shebba Mineral Fizz Buzz," +fName+" "+mName+" "+lName;
 }
