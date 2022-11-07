ce1abf5b460fede018da6d78ec1ed7bd597c0710
jchance5 <78717751+jchance5@users.noreply.github.com>
2022-11-07 18:33:11 -0500

    Update fizzbuzz2.js

--- scripts/fizzbuzz2.js
+++ scripts/fizzbuzz2.js
@@ -1,43 +1,46 @@
-"use strict";
+"use strict"
 
-function greeting() {   
-    var first_name=document.getElementById("fName").value;
-    var middle_name=document.getElementById("mName").value;
-    var last_name=document.getElementById("lName").value;
-    alert("Hello! " + fName + " " +  mName + ". " + lName + ".");
-    var firstNum = parseInt(prompt("Please enter the first number? "));
-    var secondNum = parseInt(prompt("Please enter the second number? "));
-    var values = "";
-    var numLists = 140;
 
- for (var i = 1; i <= numLists; i++) { 
-
-    if (displayWords(i,firstNum) && displayWords(i,secondNum)) {
-        values += i +". "+ "One<br>";
-    }
-    else if (displayWords(i,firstNum) && ! displayWords(i,secondNum)) {
-        values += i +". "+ "More<br>";
-    }
-    else if (! displayWords(i,firstNum) && displayWords(i,secondNum)) {
-        values += i +". "+ "Try<br>";
-    }
-    else {
-        values += i +". "+ "One more<br>";
-    }  
- } 
-    document.getElementById("mycounter").innerHTML = values;     
-       return false;     
+window.onload = function() {
+    
+   document.forms[0].onsubmit = function() {
+	  
+	  document.getElementById("greeting").innerHTML= "Welcome to GiveAnotherChance Est";
+	  var first_name=document.getElementById("fName").value;
+	  var middle_name=document.getElementById("mName").value;
+	  var last_name=document.getElementById("lName").value;
+	  var welcome=document.getElementById("greeting").textContent;
+	  document.getElementById("greeting").innerHTML = welcome +" "+ first_name +" "+ middle_name + ". " + last_name;
+	  var words = 140;
+	  
+	  let first = parseInt(document.getElementById("fNumSpin").value);
+	  let second = parseInt(document.getElementById("sNumSpin").value);
+	  var expect = "";
+	  
+	  for (var i = 1; i <= words; i++) {
+		    if (!factorFun(i,first) && !factorFun(i,second)) {
+				expect += i +".) "+ "One<br>";
+		}
+			else if (factorFun(i,first) && !factorFun(i,second)) {
+				expect +=  i +".) "+ "More<br>";
+		}
+			else if (!factorFun(i,first) && factorFun(i,second)) {
+				expect +=  i +".) "+ "Try <br>";
+		}
+			else {
+				expect +=  i +".) "+ "One more<br>";
+		}
+	  }
+	  
+	  document.getElementById("displayR").innerHTML = expect;     
+	  return false;
+   };
+};
+function factorFun(firstOne,secondTwo) {
+	if (firstOne % secondTwo == 0) {
+		return true;
+	} 
+	else {
+		return false;
+	}
 }
-function displayWords (firstNum, secondNum) {
-    if (firstNum % secondNum === 0) {
-      return true;
-      }
-      else {
-        return false;
-        }
-    }
-
-    document.getElementById("displayR").innerHTML = expect;     
-    return false;
-};
-};
