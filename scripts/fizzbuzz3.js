22630e728310e23aee4539dc37e3faa955ceb6a6
jchance5 <78717751+jchance5@users.noreply.github.com>
2022-11-21 17:14:17 -0500

    Update fizzbuzz3.js

--- scripts/fizzbuzz3.js
+++ scripts/fizzbuzz3.js
@@ -1,70 +1,80 @@
-function makeChanges()
-			{
-				changeGreeting();
-				changeLoop();
-			}
-			
-			function changeGreeting()
-			{
-				var greetingItem = document.getElementById('greeting'); // this is comment
-				var firstName = document.getElementById('firstName').value;
-				var middleInitial = document.getElementById('middleInitial').value;
-				var lastName = document.getElementById('lastName').value;
-				var newGreeting = "Welcome";
-				newGreeting = newGreeting.concat(" ", firstName);
-				if (middleInitial !== "") { newGreeting = newGreeting.concat(" ", middleInitial, "."); }
-				newGreeting = newGreeting.concat(" ", lastName, " ","to Fizz Buzz");				
-				greetingItem.textContent = newGreeting;
-				return false;
-			}
-			
-			function changeLoop()
-			{
-				var defaultMessage = "One";
-				
-				var divisorToMessage = {3 : "More", 5 : "Try", 7 : "One more!"}; // default
-				
-				var loopHeaderItem = document.getElementById('loopHeader');
-				var loopSectionItem = document.getElementById('loopSection');
-				loopHeaderItem.textContent = defaultMessage;				
-				
-				var loopLimit = 140;
-				
-				var uList = document.createElement("ol");			
-				
-				
-				var x = prompt("Enter first number",);
-				var y = prompt("Enter second number",);
-				var z = prompt("Enter second number",);
-				
-				divisorToMessage = { [x] : "One", [y] : "More", [z] : "Try!"}; 
-				
-				for (let i = 1; i <= loopLimit; i++) 				
-				{
-					var listItem = document.createElement("li");					
-					var addition = "";
-					for (let [key, value] of Object.entries(divisorToMessage)) {
-						 if (checkDivision (i, key)) {
-							addition = addition.concat(" ", value);
-						}
-					}
-					if (addition === "") {
-						addition = defaultMessage;
-					}
-					/*if ( checkDivision (i, x) && checkDivision (i, y) ) { addition = message3 + " " + message5; }
-					else if ( checkDivision (i, x) ) { addition = message3; } 
-					else if ( checkDivision (i, y) ) { addition = message5; } 
-					else { addition = message; }*/
-					listItem.appendChild(document.createTextNode(addition));
+"use strict";
 
-					uList.appendChild(listItem);
-				}
+function changehead(){
+    var fName = document.getElementById("fName").value;
+    var mName = document.getElementById("mName").value;
+    var lName = document.getElementById("lName").value;
+    document.getElementById("display").innerHTML = "Welcome to GiveAntherChance Est Fizz Buzz," +fName+" "+mName+" "+lName;
 
-				loopSectionItem.appendChild(uList);				
-			}
-			function checkDivision(x, y)
-			{
-				return (x % y == 0);
-			}
-			changeLoop();
-	
+    
+   /* let numLists = 140;*/
+    
+    var ptag = document.getElementById("nums");
+    let valueNum = "";	
+ for (var i = 1; i <= 140; i++) {
+    
+    var listparagraph = document.createElement('p');
+    
+      if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+        
+        valueNum = i.toString() + ". " + " One";
+        listparagraph.innerText = valueNum;
+        listparagraph.style.color ="white";
+        ptag.appendChild(listparagraph);
+        }
+     else if (i % 3 === 0 && i % 5 === 0) {
+            
+            valueNum = i.toString() + ". " + " More";
+            listparagraph.innerText = valueNum;
+            listparagraph.style.color ="white";
+            ptag.appendChild(listparagraph);
+        }
+     else if (i % 3 === 0 && i % 7 === 0) {
+
+            valueNum = i.toString() + ". " + " Try";
+            listparagraph.innerText = valueNum;
+            listparagraph.style.color ="white";
+            ptag.appendChild(listparagraph);
+        }
+    else if (i % 5 === 0 && i % 7 === 0) {
+           
+            valueNum = i.toString() + ". " + " Give";
+            listparagraph.innerText = valueNum;
+            listparagraph.style.color ="white";
+            ptag.appendChild(listparagraph);
+        }
+    else if (i % 3 === 0 ) {
+            
+            valueNum = i.toString() + ". " + "  Another";
+            listparagraph.innerText = valueNum;
+            listparagraph.style.color ="white";
+            ptag.appendChild(listparagraph);
+        }
+    else if (i % 5 === 0 ) {
+            
+            valueNum = i.toString() + ". " + " Chance";
+            listparagraph.innerText = valueNum;
+            listparagraph.style.color ="white";
+            ptag.appendChild(listparagraph);
+        }
+    else if (i % 7 === 0) {
+        
+        valueNum = i.toString() + ". " + " One more";
+        listparagraph.innerText = valueNum;
+        listparagraph.style.color ="white";
+        ptag.appendChild(listparagraph);
+        
+        }
+    else {
+       
+        valueNum = i.toString() + ". " + " Give another";
+        listparagraph.innerText = valueNum;
+        
+        ptag.appendChild(listparagraph); 
+         
+    }
+
+ 
+}    
+    
+}       
